4094c4f30e38e7d8c0c8aa650f5784263fb7f8d4
Cloudxier <hansenpanggabean8@gmail.com>
2025-02-01 13:53:52 +0700

    footer done

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ import "./App.css";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar />
       <Suspense>
         <Routes>
           <Route path="/" name="Landing page" element={<Home />} />
           <Route path="/about" name="About page" element={<About />} />
         </Routes>
       </Suspense>
+      <Footer />
     </div>
   );
 }
