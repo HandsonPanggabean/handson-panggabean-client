6a465f07292dfc5975941e6fb6fd1bf7ca0855c1
Cloudxier <hansenpanggabean8@gmail.com>
2025-02-02 19:35:06 +0700

    add new section contact me, need to wire the function to backend side

---
+++ src/components/inputs/InputHtmlEditor.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
+const InputHtmlEditor = (props) => {
+  const { htmlContent, setHtmlContent } = props || {};
+
+  const handleEditorChange = (newValue) => {
+    setHtmlContent(newValue);
+  };
+
+  return (
+    <div>
+      <Editor
+        apiKey={process.env.REACT_APP_TINY_MCE_API_KEY}
+        value={htmlContent}
+        onEditorChange={handleEditorChange}
+        init={{
+          height: 400,
+          menubar: false,
+          plugins: [
+            "anchor",
+            "autolink",
+            "charmap",
+            "codesample",
+            "emoticons",
+            "link",
+            "lists",
+            "searchreplace",
+            "visualblocks",
+            "wordcount",
+          ],
+          tinycomments_mode: "embedded",
+          tinycomments_author: "Author name",
+          mergetags_list: [
+            { value: "First.Name", title: "First Name" },
+            { value: "Email", title: "Email" },
+          ],
+          toolbar:
+            "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+          // ai_request: (request, respondWith) =>
+          //   respondWith.string(() =>
+          //     Promise.reject("See docs to implement AI Assistant")
+          //   ),
+        }}
+      />
+    </div>
+  );
+};
+
+export default InputHtmlEditor;
