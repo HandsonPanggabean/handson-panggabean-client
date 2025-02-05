419909de49b346df8e8c863f576b091cb2129c02
Cloudxier <hansenpanggabean8@gmail.com>
2025-02-05 21:03:48 +0700

    add ecommerce portfolio project

--- src/components/pages/Homes/PortfolioProjects.js
+++ src/components/pages/Homes/PortfolioProjects.js
@@ -46,6 +46,51 @@ import eDot_32 from "../../../assets/images/portfolioProjects/eDot/eDot_32.png";
 import eDot_33 from "../../../assets/images/portfolioProjects/eDot/eDot_33.png";
 import eDot_34 from "../../../assets/images/portfolioProjects/eDot/eDot_34.png";
 
+import ecommerce_1 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_1.png";
+import ecommerce_2 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_2.png";
+import ecommerce_3 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_3.png";
+import ecommerce_4 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_4.png";
+import ecommerce_5 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_5.png";
+import ecommerce_6 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_6.png";
+import ecommerce_7 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_7.png";
+import ecommerce_8 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_8.png";
+import ecommerce_9 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_9.png";
+import ecommerce_10 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_10.png";
+import ecommerce_11 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_11.png";
+import ecommerce_12 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_12.png";
+import ecommerce_13 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_13.png";
+import ecommerce_14 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_14.png";
+import ecommerce_15 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_15.png";
+import ecommerce_16 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_16.png";
+import ecommerce_17 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_17.png";
+import ecommerce_18 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_18.png";
+import ecommerce_19 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_19.png";
+import ecommerce_20 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_20.png";
+import ecommerce_21 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_21.png";
+import ecommerce_22 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_22.png";
+import ecommerce_23 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_23.png";
+import ecommerce_24 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_24.png";
+import ecommerce_25 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_25.png";
+import ecommerce_26 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_26.png";
+import ecommerce_27 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_27.png";
+import ecommerce_28 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_28.png";
+import ecommerce_29 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_29.png";
+import ecommerce_30 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_30.png";
+import ecommerce_31 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_31.png";
+import ecommerce_32 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_32.png";
+import ecommerce_33 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_33.png";
+import ecommerce_34 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_34.png";
+import ecommerce_35 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_35.png";
+import ecommerce_36 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_36.png";
+import ecommerce_37 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_37.png";
+import ecommerce_38 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_38.png";
+import ecommerce_39 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_39.png";
+import ecommerce_40 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_40.png";
+import ecommerce_41 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_41.png";
+import ecommerce_42 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_42.png";
+import ecommerce_43 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_43.png";
+import ecommerce_44 from "../../../assets/images/portfolioProjects/ecommerce/ecommerce_44.png";
+
 const PortfolioProjects = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -93,6 +138,57 @@ const PortfolioProjects = () => {
         { img_url: eDot_34 },
       ],
     },
+    {
+      name: "Ecommerce",
+      description: "Website app ecommerce about fashion",
+      images: [
+        { img_url: ecommerce_1 },
+        { img_url: ecommerce_2 },
+        { img_url: ecommerce_3 },
+        { img_url: ecommerce_4 },
+        { img_url: ecommerce_5 },
+        { img_url: ecommerce_6 },
+        { img_url: ecommerce_6 },
+        { img_url: ecommerce_7 },
+        { img_url: ecommerce_8 },
+        { img_url: ecommerce_9 },
+        { img_url: ecommerce_10 },
+        { img_url: ecommerce_11 },
+        { img_url: ecommerce_12 },
+        { img_url: ecommerce_13 },
+        { img_url: ecommerce_14 },
+        { img_url: ecommerce_15 },
+        { img_url: ecommerce_16 },
+        { img_url: ecommerce_17 },
+        { img_url: ecommerce_18 },
+        { img_url: ecommerce_19 },
+        { img_url: ecommerce_20 },
+        { img_url: ecommerce_21 },
+        { img_url: ecommerce_22 },
+        { img_url: ecommerce_23 },
+        { img_url: ecommerce_24 },
+        { img_url: ecommerce_25 },
+        { img_url: ecommerce_26 },
+        { img_url: ecommerce_27 },
+        { img_url: ecommerce_28 },
+        { img_url: ecommerce_29 },
+        { img_url: ecommerce_30 },
+        { img_url: ecommerce_31 },
+        { img_url: ecommerce_32 },
+        { img_url: ecommerce_33 },
+        { img_url: ecommerce_34 },
+        { img_url: ecommerce_35 },
+        { img_url: ecommerce_36 },
+        { img_url: ecommerce_37 },
+        { img_url: ecommerce_38 },
+        { img_url: ecommerce_39 },
+        { img_url: ecommerce_40 },
+        { img_url: ecommerce_41 },
+        { img_url: ecommerce_42 },
+        { img_url: ecommerce_43 },
+        { img_url: ecommerce_44 },
+      ],
+    },
   ];
   if (
     portfolioProjects &&
@@ -109,7 +205,7 @@ const PortfolioProjects = () => {
           return (
             <div
               key={index}
-              className="w-full flex flex-col justify-center bg-transparent p-4 pb-6"
+              className="w-full flex flex-col justify-center bg-transparent p-4 pb-8"
             >
               <div className="mb-3">
                 <div className="text-3xl font-bold text-gray-900 dark:text-gray-200">
@@ -137,21 +233,21 @@ const PortfolioProjects = () => {
                   className="w-full"
                   onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
                 >
-                  {images.map((image, index) => (
+                  {images.map((image, idx) => (
                     <SwiperSlide
-                      key={index}
+                      key={idx}
                       className="transition-all duration-500 flex justify-center align-center"
                     >
                       <div
                         className={`relative flex items-center justify-center bg-gray-300 dark:bg-gray-700 rounded-lg transition-all duration-500 ${
-                          index === activeIndex
+                          idx === activeIndex
                             ? "w-[323px] h-[500px] md:w-[400px] md:h-[600px] scale-100 opacity-100 z-10 -ml-24 md:-ml-14" // Active image is fully visible
                             : "w-[190px] h-[400px] md:w-[340px] md:h-[550px] scale-95 opacity-70 -translate-x-5 blur-sm z-0" // Inactive images look slightly behind
                         }`}
                       >
                         <img
                           src={image.img_url}
-                          alt={`slide-${index}`}
+                          alt={`slide-${idx}`}
                           className="w-full h-full object-contain rounded-lg"
                         />
                       </div>
