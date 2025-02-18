025a64c9907a454539d27d84bcd3bcd95fbd05ed
Cloudxier <hansenpanggabean8@gmail.com>
2025-02-18 09:27:59 +0700

    initiate AI using choosen language by user

--- src/apis/assistants.js
+++ src/apis/assistants.js
@@ -1,7 +1,7 @@
 import Axios from "../helpers/axios";
 
-export const initiateAssistant = () => {
-  return Axios.get(`/assistant/initiate`);
+export const initiateAssistant = (body) => {
+  return Axios.post(`/assistant/initiate`, body);
 };
 
 export const talkToAssistant = (body) => {
